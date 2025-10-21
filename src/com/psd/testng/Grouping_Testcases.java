b8b967cff1a74ada580c8cfae25e996ecff28825
prasad.dabhole <prasad.dabhole@IG-M-851.impactguru.net>
2025-10-21 11:01:39 +0530

    Grouping 21 Oct 2025

---
+++ src/com/psd/testng/Grouping_Testcases.java
@@ -0,0 +1,32 @@
+package com.psd.testng;
+
+import org.testng.annotations.Test;
+
+public class Grouping_Testcases {
+	@Test(groups ={"smoke"})
+	public void method1() {
+		
+	}
+	@Test
+	public void method2() {
+		
+	}
+	@Test
+	public void method3() {
+		
+	}
+	@Test
+	public void method4() {
+		
+	}
+	@Test
+	public void method5() {
+		
+	}
+	@Test
+	public void method6() {
+		
+	}
+	
+
+}
